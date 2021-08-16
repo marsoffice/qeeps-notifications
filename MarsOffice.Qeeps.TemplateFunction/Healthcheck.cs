347cd8468d57d50c4a93a7ddba0fc5220eee7d4c
Alin Florin <alin@qoffice.ro>
2021-08-16 08:55:26 +0300

    Initial commit

---
+++ MarsOffice.Qeeps.TemplateFunction/Healthcheck.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace MarsOffice.Qeeps.TemplateFunction
+{
+    public class Healthcheck
+    {
+
+        public Healthcheck()
+        {
+        }
+
+        [FunctionName("Healthcheck")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "healthcheck")] HttpRequest req)
+        {
+            await Task.CompletedTask;
+            return new OkResult();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# qeeps-templatefunction
